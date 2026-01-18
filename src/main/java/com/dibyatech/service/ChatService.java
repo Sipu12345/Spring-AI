8baf28130bf36284c6b8decfbca76db02c9d74be
Sipu123 <9sipu.977@gmail.com>
2026-01-18 14:12:05 +0530

    Integration of Gemini AI

---
+++ src/main/java/com/dibyatech/service/ChatService.java
@@ -0,0 +1,22 @@
+package com.dibyatech.service;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+
+    private final ChatClient chatClient;
+
+
+    public ChatService(ChatClient.Builder geminiChatClientBuilder) {
+        this.chatClient = geminiChatClientBuilder.build();
+    }
+
+    public String ask(String prompt){
+        return chatClient
+                .prompt(prompt)
+                .call()
+                .content();
+    }
+}
